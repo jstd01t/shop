c449af840135dbb72f79e6a719f6e6d700145cac
jstd01t <commontasksfor@gmail.com>
2022-06-01 16:07:21 +0300

    Create Model(DTO) for the form

---
+++ src/Form/DTO/EditProductModel.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Form\DTO;
+
+use App\Entity\Product;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+class EditProductModel
+{
+    /**
+     * @var int
+     */
+    public $id;
+
+    /**
+     * @var string
+     */
+    public $title;
+
+    /**
+     * @var string
+     */
+    public $price;
+
+    /**
+     * @var UploadedFile|null
+     */
+    public $newImage;
+
+    /**
+     * @var int
+     */
+    public $quantity;
+
+    /**
+     * @var string
+     */
+    public $description;
+
+    /**
+     * @var bool
+     */
+    public $isPublished;
+
+    /**
+     * @var bool
+     */
+    public $isDeleted;
+
+    public static function makeFromProduct(?Product $product): self
+    {
+        $model = new self();
+
+        if (!$product) {
+            return $model;
+        }
+
+        $model->id = $product->getId();
+        $model->title = $product->getTitle();
+        $model->price = $product->getPrice();
+        $model->quantity = $product->getQuantity();
+        $model->description = $product->getDescription();
+        $model->isPublished = $product->getIsPublished();
+        $model->isDeleted = $product->getIsDeleted();
+
+        return $model;
+    }
+}
