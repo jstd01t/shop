77cec3f0a3e9d3145160c1973d8ed0ab3299da45
jstd01t <commontasksfor@gmail.com>
2022-06-18 20:29:15 +0300

    Delete an user

--- src/Controller/Admin/UserController.php
+++ src/Controller/Admin/UserController.php
@@ -12,6 +12,7 @@ use App\Form\Handler\CategoryFormHandler;
 use App\Form\Handler\UserFormHandler;
 use App\Repository\UserRepository;
 use App\Utils\Manager\CategoryManager;
+use App\Utils\Manager\UserManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -70,12 +71,12 @@ class UserController extends AbstractController
     /**
      * @Route("/delete/{id}", name="delete")
      */
-    public function delete(Category $category, CategoryManager $categoryManager): Response
+    public function delete(User $user, UserManager $userManager): Response
     {
-        /*$categoryManager->remove($category);
+        $userManager->remove($user);
 
-        $this->addFlash('warning', 'The category was successfully deleted!');
+        $this->addFlash('warning', 'The user was successfully deleted!');
 
-        return $this->redirectToRoute('admin_category_list');*/
+        return $this->redirectToRoute('admin_user_list');
     }
 }
